6b685179798399d2ac5568ad2831ac0dcdfc9e5c
N4hu41 <lair.utp@gmail.com>
2020-02-14 16:51:19 -0600

    Segundo avance paguina de ejercicios

---
+++ js/02-Variables.js
@@ -0,0 +1,13 @@
+//Variables
+//El punto es para acceder a la propiedad de un objeto
+
+var pais = "Mexico";
+var continente = "Latino America";
+var antiguedad = 2019
+alert('Abre tu consola')
+console.log("Mostrando variables en el orden que fueron declaradas:");
+console.log('Variable país tipo string: ' + pais);
+console.log('Variable continente tipo string: ' + continente);
+console.log('Variable antigüedad tipo entero: ' + antiguedad);
+console.log('Concatenando las variables: ');
+console.log('País: ' + pais + "," + " Continente: " + continente + ','+ " Antiüedad: " + antiguedad +'.');
